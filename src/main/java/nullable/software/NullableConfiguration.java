2c287a93714b8c012a92dcd0f930a77f450c22dd
Nops <email@fake.com>
2017-10-20 01:09:00 -0700

    adding some boilerplate email code

--- src/main/java/nullable/software/NullableConfiguration.java
+++ src/main/java/nullable/software/NullableConfiguration.java
@@ -2,13 +2,27 @@ package nullable.software;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.pac4j.dropwizard.Pac4jFactory;
 
 import javax.validation.constraints.NotNull;
 
 public class NullableConfiguration extends Configuration {
-    @NotNull
-    private Pac4jFactory pac4jFactory = new Pac4jFactory();
+    @NotEmpty private String smtpHost;
+    @NotEmpty private String smtpUser;
+    @NotEmpty private String smtpPass;
+
+    @JsonProperty public String getSmtpHost() {
+        return smtpHost;
+    }
+    @JsonProperty public String getSmtpUser() {
+        return smtpUser;
+    }
+    @JsonProperty public String getSmtpPass() {
+        return smtpPass;
+    }
+
+    @NotNull private Pac4jFactory pac4jFactory = new Pac4jFactory();
 
     @JsonProperty("pac4j") public Pac4jFactory getPac4jFactory() {
         return pac4jFactory;
